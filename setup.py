9a80788dcdfb7de268dd5c76bafd82e1678346ed
pockerman <a.giavaras@gmail.com>
2023-01-23 12:20:57 +0000

    fix deprecation warning with setuptools

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='awsutils',
     packages=['awsutils'],
-    version='0.0.1',
+    version='0.0.2',
     description='Various utilities for working with AWS in Python 3.',
     author='Alexandros Giavaras',
     author_email='a.giavaras@gmail.com',
     url='https://github.com/pockerman/awsutils',
-    download_url='https://github.com/pockerman/awsutils/awsutils-0.0.1.tar.gz',
+    download_url='https://github.com/pockerman/awsutils/awsutils-0.0.2.tar.gz',
     keywords=['aws', 'image processing'],
     classifiers=[],
     scripts=[],
