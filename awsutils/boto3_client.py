2840baac24197b475a4bc5a9f71a753ef573e07e
pockerman <a.giavaras@gmail.com>
2023-01-23 12:03:16 +0000

    Add sqs client access

--- awsutils/boto3_client.py
+++ awsutils/boto3_client.py
@@ -1,25 +1,37 @@
 import boto3
+from aws_credentials import AWSCredentials_S3, AWSCredentials_SQS
 
 
-def get_aws_s3_client(region_name: str,
-                      aws_access_key_id:str,
-                      aws_secret_access_key: str):
-    """Get the AWS S3 client on the specified region
+def get_aws_s3_client(credentials: AWSCredentials_S3):
+    """Get the AWS S3 client
 
     Parameters
     ----------
-    aws_secret_access_key
-    aws_access_key_id
-    region_name
+    credentials: Credentials for accessing the S3 service
+    Returns
+    -------
+    """
+    return boto3.client('s3',
+                        aws_access_key_id=credentials.aws_access_key,
+                        aws_secret_access_key=credentials.aws_secret_access_key,
+                        region_name=credentials.aws_region)
+
+
+def get_aws_sqs_client(credentials: AWSCredentials_SQS):
+    """Get the AWS SQS client
+
+    Parameters
+    ----------
+    credentials: Credentials for accessing the SQS service
 
     Returns
     -------
 
     """
-    return boto3.client('s3',
-                        aws_access_key_id=aws_access_key_id,
-                        aws_secret_access_key=aws_secret_access_key,
-                        region_name=region_name)
+    return boto3.client('sqs',
+                        aws_access_key_id=credentials.aws_access_key,
+                        aws_secret_access_key=credentials.aws_secret_access_key,
+                        region_name=credentials.aws_region)
 
 
 
