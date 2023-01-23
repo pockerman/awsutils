1da7b3c06e117414d25d8b150421c7ec0c6281b8
pockerman <a.giavaras@gmail.com>
2023-01-23 08:16:09 +0000

    First commit for awsutils

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='awsutils',
+    packages=['awsutils'],
+    version='0.0.1',
+    description='Various utilities for working with AWS in Python 3.',
+    author='Alexandros Giavaras',
+    author_email='a.giavaras@gmail.com',
+    url='https://github.com/jrosebr1/imutils',
+    download_url='https://github.com/jrosebr1/imutils/tarball/0.1',
+    keywords=['aws', 'image processing'],
+    classifiers=[],
+    scripts=[],
+)
