865b62ab04ed6a3d8d0996c0e359e5d192b828a5
pockerman <a.giavaras@gmail.com>
2023-01-24 07:46:20 +0000

    Update setup

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
     name='awsutils',
     packages=['awsutils'],
-    version='0.0.2',
+    version='0.0.3',
     description='Various utilities for working with AWS in Python 3.',
     author='Alexandros Giavaras',
     author_email='a.giavaras@gmail.com',
     url='https://github.com/pockerman/awsutils',
-    download_url='https://github.com/pockerman/awsutils/awsutils-0.0.2.tar.gz',
-    keywords=['aws', 'image processing'],
+    download_url='https://github.com/pockerman/awsutils/awsutils-0.0.3.tar.gz',
+    keywords=['aws', 'aws-utilities'],
     classifiers=[],
     scripts=[],
 )
